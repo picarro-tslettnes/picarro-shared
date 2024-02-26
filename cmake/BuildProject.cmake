4c3ec4d3dfe832d63a6adfdce750fdb33c1ebb93
Tor Slettnes <tslettnes@picarro.com>
2024-02-25 17:49:46 -0800

    Use CMake `find_package(FOO)` instead of `PkgConfig()` for some common 3rd party dependencies as it is more universal across OS platforms

--- cmake/BuildProject.cmake
+++ cmake/BuildProject.cmake
@@ -12,9 +12,9 @@ if(NOT PROJECT_INCLUDED)
   endif()
 
   ### C++ compiler features
-  set(CMAKE_CXX_STANDARD 20)
-  set(CMAKE_CXX_STANDARD_REQUIRED ON)
-  set(CMAKE_CXX_EXTENSIONS OFF)
+  # set(CMAKE_CXX_STANDARD 20)
+  # set(CMAKE_CXX_STANDARD_REQUIRED ON)
+  # set(CMAKE_CXX_EXTENSIONS OFF)
 
   ### Source additional files from this folder
   list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}")
