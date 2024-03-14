3282c7c70db06d0d4aa0eda400c654fdccf23eb9
Tor Slettnes <tslettnes@picarro.com>
2024-03-14 11:24:50 -0700

    Revert `demoshell` to use synchronous I/O; instead provide separate asyncio example

---
+++ src/core/python/messaging/grpc/client_reader.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3 -i
+#===============================================================================
+## @file client_reader.py
+## @brief Read streamed messages from server
+## @author Tor Slettnes <tslettnes@picarro.com>
+#===============================================================================
+
+
+import threading, asyncio
+
+class ThreadReader (object):
+    '''Stream reader implementation using threads'''
+
+    thread = None
+
+    def active (self):
+        return (self.thread is not None)
+
+    def start(self, stub_reader, callback):
+        if not self.thread:
+            self.thread = threading.Thread(target = self.watch,
+                                           name = 'Watcher thread',
+                                           args = (stub_reader, callback),
+                                           daemon = True)
+            self.thread.start()
+
+    def stop(self, wait=False):
+        if thread := self.thread:
+            self.thread = None
+            if wait:
+                thread.join()
+
+    def watch(self, stub_reader, callback):
+        try:
+            for msg in stub_reader:
+                callback(msg)
+                if not self.thread:
+                    break
+        finally:
+            self.thread = None
+
+class TaskReader (object):
+    '''Stream reader implementation using AsyncIO task.'''
+
+    active = False
+
+    def active (self):
+        return self.active
+
+    async def start(self, stub_reader, callback):
+        if not self.active:
+            await self.watch(stub_reader, callback)
+
+    def stop(self, wait = False):
+        if task := self.task:
+            self.task.cancel()
+
+    async def watch(self, stub_reader, callback):
+        try:
+            self.active = True
+            async for msg in stub_reader:
+                callback(msg)
+        except asyncio.CancelledError:
+            pass
+        finally:
+            self.active = False
--- src/core/python/messaging/grpc/signal_client.py
+++ src/core/python/messaging/grpc/signal_client.py
@@ -5,10 +5,11 @@
 ## @author Tor Slettnes <tslettnes@picarro.com>
 #===============================================================================
 
+### Modules relative to install folder
 from .client  import Client
+from .client_reader import ThreadReader, TaskReader
 from picarro.io.protobuf import Picarro, ProtoBuf, SignalStore
 
-import time, threading, asyncio
 
 #===============================================================================
 # Client
@@ -121,66 +122,6 @@ class SignalClient (Client):
     signal_type = None
 
 
-    class ThreadWatcher (object):
-        '''Signal stream reader implementation using threads'''
-
-        thread = None
-
-        def active (self):
-            return (self.thread is not None)
-
-        def start(self, stub_reader, callback):
-            if not self.thread:
-                self.thread = threading.Thread(target = self.watch,
-                                               name = 'Watcher thread',
-                                               args = (stub_reader, callback))
-                self.thread.setDaemon(True)
-                self.thread.start()
-
-        def stop(self):
-            self.thread = None
-
-        def watch(self, stub_reader, callback):
-            try:
-                for msg in stub_reader:
-                    callback(msg)
-                    if not self.thread:
-                        break
-            finally:
-                self.thread = None
-
-    class TaskWatcher (object):
-        '''Signal stream reader implementation using AsyncIO task.'''
-
-        task = None
-
-        def active (self):
-            return (self.task is not None)
-
-        def start(self, stub_reader, callback):
-            import asyncio
-            if not self.task:
-                loop = asyncio.get_event_loop()
-                self.task = loop.create_task(self.watch(stub_reader, callback),
-                                             name='Watcher task')
-
-        def stop(self):
-            if self.task:
-                self.task.cancel()
-                self.task = None
-
-        async def watch(self, stub_reader, callback):
-            try:
-                async for msg in stub_reader:
-                    callback(msg)
-            except asyncio.CancelledError:
-                pass
-            finally:
-                self.task = None
-
-
-
-
     #===========================================================================
     # Instance methods
 
@@ -243,7 +184,7 @@ class SignalClient (Client):
                         **kwargs)
 
         self.signal_store = signal_store
-        self.watcher      = (self.ThreadWatcher, self.TaskWatcher)[self.use_asyncio]()
+        self.watcher = (ThreadReader, TaskReader)[self.use_asyncio]()
         if watch_all:
             self.start_watching(True)
 
@@ -276,7 +217,7 @@ class SignalClient (Client):
         if not self.watcher.active():
             sf = self.signal_filter(watch_all)
             stream = self.stream_from(self.stub.watch, sf)
-            self.watcher.start(stream, self.signal_store.emit)
+            return self.watcher.start(stream, self.signal_store.emit)
 
     def stop_watching(self):
         '''Stop watching change notifications from server'''
